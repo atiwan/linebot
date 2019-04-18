66ea9805fff013f54bb9bb34b2506b50c7e01e27
atiwan <49089182+atiwan@users.noreply.github.com>
2019-04-18 11:11:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,10 +10,11 @@ if ( sizeof($request_array['events']) > 0 ) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
         $text = $event['message']['text'];
+        $text1 = "น้ำหนักของคุณคือ ".$text;
         $data = [
             'replyToken' => $reply_token,
             // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => [['type' => 'text', 'text' => $text ]]
+            'messages' => [['type' => 'text', 'text' => $text1 ]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
